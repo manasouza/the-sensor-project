1123686af5d9553bd11a7a2697d3b3ccf739ea6e
Manassés Loiola de Souza <manasses@ciandt.com>
2020-04-11 13:48:20 -0300

    update pir to turn off LED when output is LOW

--- pir/motion_sensor.py
+++ pir/motion_sensor.py
@@ -2,34 +2,34 @@
 """
 	Detects motion and outputs a LED. 
     Credits: https://github.com/pimylifeup/motion_sensor/blob/master/motion_sensor.py
+
+    __author__ = "gus-pimylifeup"
+    __version__ = "1.0"
+    __maintainer__ = "pimylifeup.com"
 """
 
 import RPi.GPIO as GPIO
 import time
 
-__author__ = "gus-pimylifeup"
-__version__ = "1.0"
-__maintainer__ = "pimylifeup.com"
-
 pir_sensor = 11
 led = 15
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led,GPIO.OUT)
+GPIO.setup(led, GPIO.OUT)
 GPIO.setup(pir_sensor, GPIO.IN)
 
 current_state = 0
 try:
     while True:
-        # 100 ms
-        time.sleep(0.1)
+        # time in seconds
+        time.sleep(0.5)
         current_state = GPIO.input(pir_sensor)
         if current_state == 1:
-            print("GPIO pin %s is %s" % (pir_sensor, current_state))
-            GPIO.output(led,True)
-            time.sleep(1)
-            GPIO.output(led,False)
-            time.sleep(5)
+            print("GPIO pin %s is HIGH" % pir_sensor)
+            GPIO.output(led, True)
+        else:
+            print("GPIO pin %s is LOW" % pir_sensor)
+            GPIO.output(led, False)
 except KeyboardInterrupt:
     pass
 finally:
